54fe30a435e58da2f8ec96a15b694ffdebecb278
kpiculin <kristjan.piculin@gmail.com>
2022-01-09 19:28:23 +0100

    Adding GPL-v2.

--- src/main/java/com/oceanedit/Controllers/MainController.java
+++ src/main/java/com/oceanedit/Controllers/MainController.java
@@ -1,3 +1,16 @@
+// This file is part of OceanEdit.
+//
+// OceanEdit is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the FreeSoftware Foundation, either version 2 of the License, or (at
+// your option) any later version.
+//
+// OceanEdit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with OceanEdit. If not, see
+// <https://www.gnu.org/licenses/>.
+
 package com.oceanedit.Controllers;
 
 import com.oceanedit.FileOperations;
